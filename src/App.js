21483536ca4b094c27ed78831cc179530fd7f563
bakkari-sys <bkr.sana@gmail.com>
2020-04-16 03:30:19 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+import './App.css';
+import Card from './card.js'
+class App extends Component {
+  render() {
+    return (
+   <Card/>
+    );
+  }
+}
+
+export default App;
